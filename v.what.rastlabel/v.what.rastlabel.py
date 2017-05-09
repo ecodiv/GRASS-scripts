030cb760f59aeae1cab670d140a43a37c74d0ebb
ecodiv <paulo@ecodiv.org>
2017-05-09 15:32:17 +0200

    addon v.what.rastlabel: added option to write raster values and labels to input vector layer. Removing redundant code.

--- v.what.rastlabel/v.what.rastlabel.py
+++ v.what.rastlabel/v.what.rastlabel.py
@@ -4,11 +4,11 @@
 ##############################################################################
 #
 # MODULE:       v.what.rastlabel
-# AUTHOR(S):    Paulo van Breugel <paulo at ecodiv.org>
+# AUTHOR(S):    Paulo van Breugel <paulo at ecodiv.earth>
 # PURPOSE:      Upload raster values and labels at positions of vector points
 #               to the vector attribute table.
 #
-# COPYRIGHT: (C) 2016 by Paulo van Breugel and the GRASS Development Team
+# COPYRIGHT: (C) 2016-2017 by Paulo van Breugel and the GRASS Development Team
 #
 #        This program is free software under the GNU General Public
 #        License (>=v2). Read the file COPYING that comes with GRASS
@@ -49,7 +49,17 @@
 #%option G_OPT_V_OUTPUT
 #% description: Name of output point layer
 #% key_desc: name
-#% required: yes
+#% required: no
+#%end
+
+#%flag
+#% key: o
+#% description: add columns to input vector map
+#% required: no
+#%end
+
+#%rules
+#% required: output,-o
 #%end
 
 # import libraries
@@ -57,9 +67,26 @@ import os
 import sys
 import grass.script as gs
 import string
+import uuid
 from grass.pygrass.modules import Module
 from subprocess import PIPE
 import tempfile
+
+# create set to store names of temporary maps to be deleted upon exit
+CLEAN_RAST = []
+
+
+# Create temporary name
+def tmpname(prefix):
+    """Generate a tmp name which contains prefix
+    Store the name in the global list.
+    Use only for raster maps.
+    """
+    tmpf = prefix + str(uuid.uuid4())
+    tmpf = string.replace(tmpf, '-', '_')
+    CLEAN_RAST.append(tmpf)
+    return tmpf
+
 
 def main(options, flags):
 
@@ -79,6 +106,9 @@ def main(options, flags):
     RASTL2 = [x.lower() for x in RASTL2]
     VECT = options['vector']
     OUTP = options['output']
+    if not OUTPUT:
+        OUTPUT = tmpname("v_what_rastlabel")
+    flag_o = flags['o']
 
     # Create vector with column names
     CT = ['x double precision, y double precision, label integer']
@@ -96,7 +126,7 @@ def main(options, flags):
     # Export point map to text file first and use that as input in r.what
     # TODO: the above is workaround to get vector cat value as label. Easier,
     # would be to use vector point map directly as input, but that does not
-    # give label to link back to old vector layer
+    # give label to link back to old vector layer. Suggestions welcome.
     PAT = Module('v.out.ascii', input=VECT, format='point', separator='space',
                  precision=12, stdout_=PIPE).outputs.stdout
     CAT = Module('r.what', flags='f', map=RAST, stdin_=PAT,
@@ -108,14 +138,13 @@ def main(options, flags):
     # Get raster points of raster layers without labels (optional)
     if options['raster2']:
         for j in xrange(len(RAST2)):
-            DT = gs.parse_command('r.info', flags='g', map=RAST2[j],
-                                  quiet=True)['datatype']
-            if DT == 'CELL':
-                CT = "{} integer".format(RASTL2[j])
-            else:
-                CT = "{} double precision".format(RASTL2[j])
-            Module('v.db.addcolumn', map=OUTP, columns=CT)
             Module('v.what.rast', map=OUTP, raster=RAST2[j], column=RASTL2[j])
+
+    # Join table to original layer (without x,y columns)
+    if flag_o:
+        cols = CNT + RASTL2
+        Module('v.db.join', map=VECT, column='cat', other_table=OUTP,
+               other_column='label', subset_columns=cols)
 
     # Write metadata
     opt2 = dict((k, v) for k, v in options.iteritems() if v)
@@ -125,4 +154,5 @@ def main(options, flags):
            cmdhist=hist, flags='r', quiet=True)
 
 if __name__ == "__main__":
+    atexit.register(cleanup)
     sys.exit(main(*gs.parser()))
